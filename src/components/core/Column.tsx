1bdf5da38523c2692b0de9b62ad662a4db30122f
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-20 15:00:22 +0700

    Create Column.tsx

---
+++ src/components/core/Column.tsx
@@ -0,0 +1,22 @@
+interface ColumnProps {
+  children: React.ReactNode;
+  classNames?: string;
+  onClick?: () => void;
+  id?: string;
+  elementRef?: React.RefObject<HTMLDivElement>;
+}
+
+const Column = ({ children, classNames = "", onClick, id, elementRef }: ColumnProps) => {
+  return (
+    <div
+      className={`relative flex flex-col justify-start items-start transition duration-300 ease-in-out ${classNames}`}
+      id={id}
+      ref={elementRef}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Column; 
