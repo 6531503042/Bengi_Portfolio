364074612d7cde47b2af1b6847704c05adf7984e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-20 15:00:23 +0700

    Create Row.tsx

---
+++ src/components/core/Row.tsx
@@ -0,0 +1,22 @@
+interface RowProps {
+  children: React.ReactNode;
+  classNames?: string;
+  onClick?: () => void;
+  id?: string;
+  elementRef?: React.RefObject<HTMLDivElement>;
+}
+
+const Row = ({ children, classNames = "", onClick, id, elementRef }: RowProps) => {
+  return (
+    <div
+      className={`relative flex flex-row justify-start items-start transition duration-300 ease-in-out ${classNames}`}
+      id={id}
+      ref={elementRef}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Row; 
