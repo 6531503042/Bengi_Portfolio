f77930c3c4fdbbb60a40a243d5fcac9004ec45ab
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-20 16:12:18 +0700

    Create GridBox.tsx

---
+++ src/components/core/GridBox.tsx
@@ -0,0 +1,22 @@
+interface GridBoxProps {
+  children: React.ReactNode;
+  classNames?: string;
+  onClick?: () => void;
+  id?: string;
+  elementRef?: React.RefObject<HTMLDivElement>;
+}
+
+const GridBox = ({ children, classNames = "", onClick, id, elementRef }: GridBoxProps) => {
+  return (
+    <div
+      className={`relative w-full grid gap-6 transition duration-300 ease-in-out ${classNames}`}
+      id={id}
+      ref={elementRef}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GridBox; 
