ba8615cdebd72cea30108673748c5c5080a8cef7
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-20 15:13:12 +0700

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,30 @@
 import { LucideIcon } from "lucide-react";
 
+export enum RepoType {
+  Public = "Public",
+  Private = "Private"
+}
+
+export enum ProjectType {
+  Personal = "Personal",
+  JobWork = "JobWork",
+  Freelance = "Freelance"
+}
+
+export interface IProjectItem {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  repoType: RepoType;
+  projectType: ProjectType;
+  githubUrl?: string;
+  url?: string;
+  playStore?: string;
+  tags: string[];
+  screenshots?: string[];
+}
+
 export interface Link {
   name: string;
   href: string;
