1d023405c220b8de1e997a6ebb747c7de98724c9
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-26 21:48:39 +0700

    Update HeroSection.tsx

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -114,9 +114,9 @@ const HeroSection = () => {
               <div className="absolute inset-0 bg-gradient-to-br from-blue-100 to-purple-100 rounded-3xl -rotate-6 transform transition-transform duration-300 hover:rotate-0"></div>
               <div className="absolute inset-0 bg-gradient-to-tr from-blue-50 to-purple-50 rounded-3xl rotate-3 transform transition-transform duration-300 hover:rotate-0"></div>
               <img
-                src="./public/images/not_found.png"
+                src="./public/Bengi.jpg"
                 alt="Profile"
-                className="relative w-full h-full rounded-2xl object-cover shadow-xl transition-transform duration-300 hover:scale-105"
+                 className="relative w-full h-full rounded-2xl object-cover object-[60%] shadow-xl transition-transform duration-300 hover:scale-105"
               />
             </motion.div>
           </motion.div>
